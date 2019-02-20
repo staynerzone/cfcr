20052ae2c5b89a9257b07b00e0926c8a8626b5d6
ptFive <johannes.steiner@protonmail.com>
2019-02-20 17:15:11 +0000

    extending task

--- tasks/fetch-token/task.sh
+++ tasks/fetch-token/task.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -x
+set -e
 
+echo "CONNECTING..."
 ./cfcr-git/bin/set_kubeconfig_from_concourse $DIRECTOR_NAME/cfcr https://$CFCR_MASTER_DNS:8443
+echo "...CONNECTED!"
+
+echo "EXPORTING TOKEN..."
+cat ~/.kube/config | grep token | awk '{print $2}' > k8s-token/token
+echo "...DONE"
+
+credhub
