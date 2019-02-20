1e279d5d34f37c36651570f9983491acda097dcf
ptFive <johannes.steiner@protonmail.com>
2019-02-20 17:30:21 +0000

    added credhub set logic

--- tasks/fetch-token/task.sh
+++ tasks/fetch-token/task.sh
@@ -6,8 +6,8 @@ echo "CONNECTING..."
 ./cfcr-git/bin/set_kubeconfig_from_concourse $DIRECTOR_NAME/cfcr https://$CFCR_MASTER_DNS:8443
 echo "...CONNECTED!"
 
-echo "EXPORTING TOKEN..."
-cat ~/.kube/config | grep token | awk '{print $2}' > k8s-token/token
+echo "FETCHING TOKEN..."
+cat ~/.kube/config | grep token | awk '{print $2}' > token
 echo "...DONE"
 
 {
@@ -16,3 +16,8 @@ echo "...DONE"
 
 credhub api $CREDHUB_URL --ca-cert ca-cert
 credhub login
+credhub set -n /concourse/main/cfcr-deployment/token -t value -v `cat token`
+credhub logout
+
+rm ca-cert
+rm token
