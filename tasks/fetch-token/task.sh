f9a3db5cf67d4dd67f905c67fa4fd713e569b2c6
ptFive <johannes.steiner@protonmail.com>
2019-02-20 16:52:54 +0000

    edit task fetch-token to use set_kubeconfig_from_concourse:

--- tasks/fetch-token/task.sh
+++ tasks/fetch-token/task.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 
-./cfcr-git/bin/set_kubeconfig $DIRECTOR_NAME/cfcr https://$CFCR_MASTER_DNS:8443
+./cfcr-git/bin/set_kubeconfig_from_concourse $DIRECTOR_NAME/cfcr https://$CFCR_MASTER_DNS:8443
